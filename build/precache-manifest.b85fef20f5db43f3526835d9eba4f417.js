6eb356970616c826ea267311c7d1627ee137fc5e
invincible0809 <sughoshpdixit@gmail.com>
2020-08-10 13:18:53 +0530

    env changes

---
+++ build/precache-manifest.b85fef20f5db43f3526835d9eba4f417.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6f2fd561eceacb8384a66c5f588068",
+    "url": "/SughoshDixit/index.html"
+  },
+  {
+    "revision": "866293b2390723ebc0f6",
+    "url": "/SughoshDixit/static/css/3.b499fcd2.chunk.css"
+  },
+  {
+    "revision": "05b5fe693125fef400ba",
+    "url": "/SughoshDixit/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "20d2d1536b40fc50b33b",
+    "url": "/SughoshDixit/static/css/main.3ed128e5.chunk.css"
+  },
+  {
+    "revision": "914e29bea37c93f4beb5",
+    "url": "/SughoshDixit/static/js/2.67cc4b7f.chunk.js"
+  },
+  {
+    "revision": "866293b2390723ebc0f6",
+    "url": "/SughoshDixit/static/js/3.cda9b5f2.chunk.js"
+  },
+  {
+    "revision": "05b5fe693125fef400ba",
+    "url": "/SughoshDixit/static/js/4.211d8230.chunk.js"
+  },
+  {
+    "revision": "20d2d1536b40fc50b33b",
+    "url": "/SughoshDixit/static/js/main.b36d7ba8.chunk.js"
+  },
+  {
+    "revision": "a07ff3f4890761782f40",
+    "url": "/SughoshDixit/static/js/runtime-main.9c3c835e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/SughoshDixit/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/SughoshDixit/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "7b9044131c21c4f36c2805c8995c9e7a",
+    "url": "/SughoshDixit/static/media/Rakuten.7b904413.jpg"
+  },
+  {
+    "revision": "34dfe38021673ee47b8bdcae2178d33e",
+    "url": "/SughoshDixit/static/media/contactMail.34dfe380.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/SughoshDixit/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/SughoshDixit/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "933caf72063b6eedcc05f04ecc6f2bc0",
+    "url": "/SughoshDixit/static/media/logo.933caf72.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/SughoshDixit/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "f5b9c664252b737d150f8cba76a5972c",
+    "url": "/SughoshDixit/static/media/paniit.f5b9c664.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/SughoshDixit/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/SughoshDixit/static/media/talksCardBack.de3da806.svg"
+  }
+]);
